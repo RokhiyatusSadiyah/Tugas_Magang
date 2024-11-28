22af9415e9a006df3d90305e308d4a297fce69ba
RokhiyatusSadiyah <adeksaya976@gmail.com>
2024-11-28 16:08:57 +0700

    Repair Searching

--- lib/controller/dashboard_controller.dart
+++ lib/controller/dashboard_controller.dart
@@ -1,10 +1,27 @@
+// import 'package:get/get.dart';
+//
+// class DashboardController extends GetxController {
+//   var tabIndex = 0;
+//
+//   void updateIndex(int index){
+//     tabIndex =index;
+//     update();
+//   }
+// }
 import 'package:get/get.dart';
 
 class DashboardController extends GetxController {
-  var tabIndex = 0;
+  int tabIndex = 0;
+  bool isSearching = false;
 
-  void updateIndex(int index){
-    tabIndex =index;
+  void updateIndex(int index) {
+    tabIndex = index;
+    isSearching = false; // Reset search mode when switching tabs
+    update();
+  }
+
+  void toggleSearch() {
+    isSearching = !isSearching;
     update();
   }
 }
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../component/main_header.dart'; // Import MainHeader
+import 'package:tugas_magang/view/home/search_and_cart.dart' as search_and_cart; // Alias for search_and_cart
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,13 +10,38 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          MainHeader(), // Use MainHeader widget here
+          MainHeader(), // Main header (with badge)
+          const SizedBox(height: 20),
+          search_and_cart.LongSearchBox(), // Use alias to call LongSearchBox
+          const SizedBox(height: 20),
+          search_and_cart.CartBadge(), // Use alias to call CartBadge
           // You can add more widgets below as needed
         ],
       ),
     );
   }
 }
+
+
+//Kode Lama
+// import 'package:flutter/material.dart';
+// import '../../component/main_header.dart'; // Import MainHeader
+//
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Column(
+//         children: [
+//           MainHeader(), // Use MainHeader widget here
+//           // You can add more widgets below as needed
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 
 // import 'package:flutter/material.dart';
